337b4fce7289a632a211a83a01620f16c936d274
OctaMostro <127501081+OctaMostro@users.noreply.github.com>
2023-11-13 17:09:59 -0300

    Add files via upload

---
+++ JUEGO-JS-FINAAAAL/menu.js
@@ -0,0 +1,9 @@
+        document.getElementById("creditos").addEventListener("click", function() {
+        document.getElementById("MENU-INICIO").style.display = "none";
+        document.getElementById("creditos-menu").style.display = "block";
+    });
+
+        document.getElementById("boton-volver").addEventListener("click", function() {
+        document.getElementById("MENU-INICIO").style.display = "block";
+        document.getElementById("creditos-menu").style.display = "none";
+    });
